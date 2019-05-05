3f251349ad0c6315f50a1fc032faa51e2f57a797
Tommi <tommi.hovi@gmail.com>
2019-05-05 20:44:17 +0300

    Part 1 checkpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,5 +8,10 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
