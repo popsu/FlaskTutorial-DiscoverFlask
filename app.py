a6a8a9463dd6efd42776f7dd95f14b4dcf9b4454
Tommi <tommi.hovi@gmail.com>
2019-05-05 21:31:52 +0300

    Part 3 checkpoint

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
+# config
+app.secret_key = 'my_precious'
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+
+    return wrap
+
 
 @app.route('/')
+@login_required
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -20,9 +38,20 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid credentials. Please try again.'
         else:
+            session['logged_in'] = True
+            flash('You were just logged in!')
             return redirect(url_for('home'))
 
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+
+    flash('You were just logged out!')
+    return redirect(url_for('welcome'))
 
 
 if __name__ == '__main__':
