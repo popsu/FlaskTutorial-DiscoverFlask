e97aa4a27ac3cbf4fb3695535a7b037dfdc6345a
Tommi Hovi <tommi.hovi@gmail.com>
2019-05-18 20:35:52 +0300

    Fix column types

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ class BlogPost(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
 
     def __init__(self, title, description):
         self.title = title
