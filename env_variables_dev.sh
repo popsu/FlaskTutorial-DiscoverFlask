738cc6fda8202d8afadf0347fac9ceaf3e12c0a7
Tommi Hovi <tommi.hovi@gmail.com>
2019-05-19 13:45:58 +0300

    Part 17 checkpoint

---
+++ env_variables_dev.sh
@@ -0,0 +1,3 @@
+# If using virtualenvwrapper add the lines below to $VIRTUAL_ENV/bin/postactivate file
+export APP_SETTINGS='config.DevelopmentConfig'
+export DATABASE_URL='postgresql:///discover_flask_dev'
