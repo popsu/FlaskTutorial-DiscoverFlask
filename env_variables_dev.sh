3db646bf6c653bfe20acebea08f2cdff4031b778
Tommi Hovi <tommi.hovi@gmail.com>
2019-05-21 00:00:03 +0300

    Add more config

--- env_variables_dev.sh
+++ env_variables_dev.sh
@@ -1,3 +1,5 @@
 # If using virtualenvwrapper add the lines below to $VIRTUAL_ENV/bin/postactivate file
 export APP_SETTINGS='config.DevelopmentConfig'
 export DATABASE_URL='postgresql:///discover_flask_dev'
+export FLASK_APP="run.py"
+export FLASK_DEBUG=1
